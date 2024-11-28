c7f26140f73592f53db9ae901b8b5a525e574af6
sakuma-shin <k023g0084@g.neec.ac.jp>
2024-11-28 18:48:07 +0900

    01_00完了

--- Particle.PS.hlsl
+++ Particle.PS.hlsl
@@ -18,10 +18,15 @@ SamplerState gSampler : register(s0);
 PixelShaderOutput main(VertexShaderOutput input)
 {
     
-    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
+    
 
     PixelShaderOutput output;
+    float32_t4 textureColor = gTexture.Sample(gSampler, input.texcoord);
     output.color = gMaterial.color * textureColor;
+    if (output.color.a == 0)
+    {
+        discard;
+    }
     
     
     return output;
--- Particle.VS.hlsl
+++ Particle.VS.hlsl
@@ -4,6 +4,8 @@
 struct TransformationMatrix
 {
     float32_t4x4 WVP;
+    
+    float32_t4x4 World;
 };
 StructuredBuffer<TransformationMatrix> gTransformationMatrices : register(t0);
 
@@ -21,9 +23,9 @@ struct VertexShaderInput
 VertexShaderOutput main(VertexShaderInput input,uint32_t instanceId:SV_InstanceID)
 {
     VertexShaderOutput output;
-    output.position = mul(input.position, (float32_t3x3) gTransformationMatrices[instanceId].WVP);
+    output.position = mul(input.position,gTransformationMatrices[instanceId].WVP);
     output.texcoord = input.texcoord;
-    output.normal = normalize(mul(input.position, (float32_t3x3) gTransformationMatrices[instanceId].World));
+    //output.normal = normalize(mul(input.position, (float32_t3x3) gTransformationMatrices[instanceId].World));
     return output;
 }
 
