424352c616bc498bb116ed78e9bd3d05fffd9c96
sakuma-shin <k023g0084@g.neec.ac.jp>
2025-01-31 10:37:48 +0900

    テクスチャが貼られない問題を修正

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -40,8 +40,8 @@ PixelShaderOutput main(VertexShaderOutput input)
         float NDotH = dot(normalize(input.normal), halfVector);
         float specularPow = pow(saturate(NDotH), gMaterial.shininess);
     
-        float32_t3 diffuse = gMaterial.color.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
-    
+        float32_t3 diffuse = gMaterial.color.rgb * textureColor.rgb * gDirectionalLight.color.rgb * cos * gDirectionalLight.intensity;
+        // 鏡面反射
         float32_t3 specular = gDirectionalLight.color.rgb * gDirectionalLight.intensity * specularPow * float32_t3(1.0f, 1.0f, 1.0f);
     
         output.color.rgb = diffuse + specular;
